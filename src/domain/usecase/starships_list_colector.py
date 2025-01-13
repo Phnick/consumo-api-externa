e2bc9764361f2e1f8bf6145580ea085bf840ad1c
Phnick <phnikidefaria3@gmail.com>
2025-01-13 16:45:41 -0300

    third commit

--- src/domain/usecase/starships_list_colector.py
+++ src/domain/usecase/starships_list_colector.py
@@ -1,8 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import Dict, List
 
-# classes abstratas a gente nao instacia e so herda
-
 
 class StarshipsColectorInterface(ABC):
     '''Starships colector interfaces'''
